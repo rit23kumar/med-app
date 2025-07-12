65d219f45b962c3f0a6f6d512643fc3423768112
Ritesh Kumar <rit23kumar@gmail.com>
2025-07-13 00:14:10 +0530

    Remove unnecessary whitespace in MedCategoryRepository.java to maintain code consistency.

--- src/main/java/com/medapp/repository/MedCategoryRepository.java
+++ src/main/java/com/medapp/repository/MedCategoryRepository.java
@@ -2,6 +2,6 @@ package com.medapp.repository;
 
 import com.medapp.entity.MedCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+ 
 public interface MedCategoryRepository extends JpaRepository<MedCategory, Integer> {
 } 
